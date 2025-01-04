d17348d929878cc4f6b3e214a94eaaca92118167
gregory <gregory.ledenev@gmail.com>
2025-01-04 13:31:09 -0500

    - tests update

--- tests/com/gl/classext/UnionTest.java
+++ tests/com/gl/classext/UnionTest.java
@@ -15,7 +15,6 @@ public class UnionTest {
             return name + suffix;
         }
     }
-
     public record Furniture(String name) {
         public String getName() {
             return name;
@@ -25,7 +24,6 @@ public class UnionTest {
             return name + suffix;
         }
     }
-
     public record ElectronicItem(String name) {
         public String getName() {
             return name;
@@ -35,7 +33,6 @@ public class UnionTest {
             return name + suffix;
         }
     }
-
     public record AutoPart(String name) {
         public String getName() {
             return name;
@@ -45,14 +42,11 @@ public class UnionTest {
             return name + suffix;
         }
     }
-
     public record ShippingInfo(String result) {}
 
     @ExtensionInterface
     public interface Shippable {
         String name();
-        String getName();
-        String getName(String suffix);
         ShippingInfo ship();
     }
 
@@ -63,9 +57,6 @@ public class UnionTest {
                         operation(Furniture.class, furniture -> new ShippingInfo(furniture.name() + " furniture shipped")).
                         operation(ElectronicItem.class, electronicItem -> new ShippingInfo(electronicItem.name() + " electronic item shipped")).
                         operation(AutoPart.class, electronicItem -> new ShippingInfo(electronicItem.name() + " auto part shipped")).
-                    operationName("getName").
-                        operation(Object.class, (object) -> DynamicClassExtension.performOperation("getName", object)).
-                        operation(Object.class, (Object object, String suffix) -> DynamicClassExtension.performOperation("getName", object, suffix)).
                     operationName("name").
                         operation(Object.class, (object) -> DynamicClassExtension.performOperation("name", object)).
                 build();
@@ -81,36 +72,10 @@ public class UnionTest {
                 new AutoPart("Tire"),
         };
 
-        StringBuilder shippingInfos = new StringBuilder();
         for (Object item : items) {
             Shippable extension = dynamicClassExtension.extension(item, Shippable.class);
-            if (!shippingInfos.isEmpty())
-                shippingInfos.append("\n");
-            ShippingInfo ship = extension.ship();
-            shippingInfos.append(ship).append("\n");
-            shippingInfos.append(extension.name()).append("\n");
-            shippingInfos.append(extension.getName()).append("\n");
-            shippingInfos.append(extension.getName("123"));
+            out.println("Shipping: " + extension.name());
+            extension.ship();
         }
-        out.println(shippingInfos);
-
-        assertEquals("""
-                                ShippingInfo[result=The Mythical Man-Month book shipped]
-                                The Mythical Man-Month
-                                The Mythical Man-Month
-                                The Mythical Man-Month123
-                                ShippingInfo[result=Sofa furniture shipped]
-                                Sofa
-                                Sofa
-                                Sofa123
-                                ShippingInfo[result=Soundbar electronic item shipped]
-                                Soundbar
-                                Soundbar
-                                Soundbar123
-                                ShippingInfo[result=Tire auto part shipped]
-                                Tire
-                                Tire
-                                Tire123""",
-                shippingInfos.toString());
     }
 }
